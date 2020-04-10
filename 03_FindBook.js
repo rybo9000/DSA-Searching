6fd1cfc87f80d2a642f6979bbd1ad76e44c9d098
Ryan Fielder <ryan@FIELDER.LOCAL>
2020-04-10 17:08:42 -0500

    updated work

---
+++ 03_FindBook.js
@@ -0,0 +1,22 @@
+function deweySearching(dewey, title, start, end) {
+  start = start === undefined ? 0 : start;
+  end = end === undefined ? dewey.length : end;
+
+  if (start > end) {
+    return -1;
+  }
+  const index = Math.floor((start + end) / 2);
+  const item = dewey[index];
+
+  for (let i = 0; i < dewey.length; i++) {
+    if (dewey[i] == title) {
+      return "found book";
+    }
+  }
+  if (item < dewey) {
+    return deweySearching(dewey, title, index + 1, end);
+  } else if (item > dewey) {
+    return deweySearching(dewey, title, start, index - 1);
+  }
+}
+console.log(deweySearching(["be", "he", "hi"], "hi"));
